9de61abf3c7c81ed71522b9fcc87252b1952746d
Neeraj Jagga <neerajjagga26@gmail.com>
2025-06-20 00:15:40 +0530

    added common db with prisma

--- apps/http-server/src/controllers/auth.controller.ts
+++ apps/http-server/src/controllers/auth.controller.ts
@@ -1,10 +1,19 @@
 import { type Request, type Response } from "express";
 import { JWT_SECRET } from '@repo/backend-common/config';
+import { prisma } from '@repo/db/prisma';
 
-export const signupUser = (req: Request, res: Response) => {
+export const signupUser = async (req: Request, res: Response) => {
     try {
         const { name, email, password } = req.body;
         console.log(JWT_SECRET);
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password
+            }
+        });
         
     } catch (error) {
         
